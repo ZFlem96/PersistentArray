0280ff2a7823c9d38bd98cf5f960548ecc3f2f7e
ZFlem96 <zackzacharyfleming@gmail.com>
2016-07-19 19:37:12 -0600

    First commit

---
+++ PersistentArray/src/app/Driver.java
@@ -0,0 +1,22 @@
+package app;
+
+//import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.net.*;
+
+public class Driver {
+	
+	
+
+	public static void main(String[] args) {
+//		Crawler c = new Crawler();
+//		c.toVisit = new ArrayList<URL>();
+//		c.hasVisited = new ArrayList<URL>();
+//		ArrayList<URL> toVisit = c.toVisit;
+//		ArrayList<URL> hasVisited = c.hasVisited;
+	}
+}
